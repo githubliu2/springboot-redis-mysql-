aa51f85f984013f77920a81a2102cbd40c06ad11
lm <1770871398@qq.com>
2022-02-16 10:14:40 +0800

    controller添加测试快速排序的方法

--- src/main/java/com/spring/test/RedisClientTest.java
+++ src/main/java/com/spring/test/RedisClientTest.java
@@ -4,6 +4,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.spring.entity.User;
+import com.spring.service.QuickSortSerice;
 import com.spring.util.RedisUtil;
 
 import redis.clients.jedis.Jedis;
@@ -28,6 +30,8 @@ public class RedisClientTest {
 	 private RedisUtil redisUtil;
 	 @Autowired
 	 private JdbcTemplate jdbcTemplate; 
+	 @Autowired
+	 private QuickSortSerice quickSortSerice;
  
     @GetMapping(value = "getUserByRedis")
     public String getIndex(){
@@ -85,5 +89,34 @@ public class RedisClientTest {
              System.out.println("查询成功："  +  users);
         }
         return jedisResult;
+    }
+    
+    @GetMapping(value = "sortRanuomList")
+    public String sortRanuomList(Integer size){
+    	Random rand = new Random();
+    	
+    	int[] waitSortList = new int[size];
+    	for (int i = 0; i < waitSortList.length; i++) {
+    		waitSortList[i] = rand.nextInt(size);
+    	}
+    	
+    	StringBuffer preSortList = new StringBuffer("排序前的数组: \n");
+    	System.out.println("排序前的数组: ");
+    	for (int i : waitSortList) {
+    		System.out.print(i + ",");
+    		preSortList.append(i + ",");
+    	}
+    	
+    	quickSortSerice.quickSort(waitSortList, 0, size - 1);
+    	
+    	StringBuffer afterSortList = new StringBuffer("\n排序后的数组: \n");
+    	System.out.println("排序后的数组: ");
+    	for (int i : waitSortList) {
+    		System.out.print(i + ",");
+    		afterSortList.append(i + ",");
+    	}
+    	
+		return preSortList.append(afterSortList).toString();
+    	
     }
 }
